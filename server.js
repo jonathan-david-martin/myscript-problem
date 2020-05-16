877840ffb803f1e7ff1fc7384b2d075e1a8d131b
Jon Martin <jonathanmartinnyc@gmail.com>
2020-05-16 02:02:51 -0400

    Revert "testing"
    This reverts commit b25e0cf1e1e6357efd69982fcf72065b13120d0f.

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ var port = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
 //app.use(express.static(__dirname));
-app.use(express.static('./build'));
+app.use(express.static('./build/default'));
 
 var http = require('http');
 var server = http.Server(app);
 var io = require('socket.io')(server);
 
 app.get('/', function (req, res) {
-    res.sendfile('./build/index.html');
+    res.sendfile('./build/default/index.html');
 });
 
 server.listen(port, hostname, function () {
