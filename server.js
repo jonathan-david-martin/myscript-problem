b25e0cf1e1e6357efd69982fcf72065b13120d0f
Jon Martin <jonathanmartinnyc@gmail.com>
2020-05-16 01:59:37 -0400

    testing

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ var port = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
 //app.use(express.static(__dirname));
-app.use(express.static('./build/default'));
+app.use(express.static('./build'));
 
 var http = require('http');
 var server = http.Server(app);
 var io = require('socket.io')(server);
 
 app.get('/', function (req, res) {
-    res.sendfile('./build/default/index.html');
+    res.sendfile('./build/index.html');
 });
 
 server.listen(port, hostname, function () {
