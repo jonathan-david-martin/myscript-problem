0483201a688559c562522decd867b3d162ab7a2f
Jon Martin <jonathanmartinnyc@gmail.com>
2020-05-16 01:42:57 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+var hostname = '0.0.0.0';
+var port = process.env.PORT || 3000;
+var express = require('express');
+var app = express();
+//app.use(express.static(__dirname));
+app.use(express.static('./build/default'));
+
+var http = require('http');
+var server = http.Server(app);
+var io = require('socket.io')(server);
+
+app.get('/', function (req, res) {
+    res.sendfile('index.html');
+});
+
+server.listen(port, hostname, function () {
+    console.log('listening on ' + hostname + ':' + port);
+});
