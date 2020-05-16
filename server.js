3b873710ec4b728ef3b315b5fed1506a16c37d8a
Jon Martin <jonathanmartinnyc@gmail.com>
2020-05-16 10:30:33 -0400

    path updates

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ var port = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
 //app.use(express.static(__dirname));
-app.use(express.static('./build/default'));
+app.use('/', express.static(path.join(__dirname, '/build/default')));
+//app.use(express.static('./build/default'));
 
 var http = require('http');
 var server = http.Server(app);
 var io = require('socket.io')(server);
 
 app.get('/', function (req, res) {
-    res.sendfile('./build/default/index.html');
+    res.sendfile('index.html');
 });
 
 server.listen(port, hostname, function () {
